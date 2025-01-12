a833398d4a3217b805481ebcd8e611b01d46f32f
LitzJ <2609008122@qq.com>
2025-01-13 01:56:22 +0800

    更改构建工具 现在使用CMake

--- modules/master_machine/rv2_protocal.h
+++ modules/master_machine/rv2_protocal.h
@@ -5,21 +5,23 @@
 #ifndef RV2_PROTOCAL_H
 #define RV2_PROTOCAL_H
 
+#include "main.h"
+#include "stdbool.h"
+
 #pragma pack(1)
 typedef struct ReceivePacket
 {
-    uint8_t header = 0x5A;
-    uint8_t detect color :1; // 0-red 1-blue
+    uint8_t header ;//0x5A
+    uint8_t detect_color :1; // 0-red 1-blue
     bool reset_tracker :1;
     uint8_t reserved :6;
 float roll;
 float pitch;
 float yaw;
-float aim x;
-float aim y;
-float aim z;
-uint16 t checksum = 0;
-
+float aim_x;
+float aim_y;
+float aim_z;
+uint16_t checksum;
 } rv2_protocol_s;
 
 #pragma pack()
